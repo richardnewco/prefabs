f79e9840d566d65855b339c2614d59fb05a311b9
Richard Newcombe <hirich1@hotmail.com>
2020-10-17 13:21:30 +1100

    prefabs

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float bulletSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody>().AddForce(Vector3.forward * bulletSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float randX, randY, randZ;
+
+    public Vector3 randPos, randPosW;
+
+    public GameObject bulletPrefab;
+
+    private GameObject bulletToDie;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+        if (Input.GetKeyDown("space"))
+        {
+            randPosW = RandPos();
+           bulletToDie = Instantiate(bulletPrefab, randPosW, Quaternion.identity);
+
+        }
+
+        Vector3 RandPos()
+        {
+            randX = Random.Range(-4f, 4f);
+            randY = 4.74f;
+            randZ = -29.69f;
+
+            randPos = new Vector3(randX, randY, randZ);
+
+            return randPos;
+
+        }
+        Destroy(bulletToDie, 3.0f);
+    }
+}
+
